71b0cdfc82b37c1fc5eed89201c7e05a60a5e55c
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-05 12:53:46 +0200

    small issue with err handler fixed 05.05.23 12.53

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,8 +1,13 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export class ValidationError extends Error {}
 
-export const handleError = (err: Error, req: Request, res: Response) => {
+export const handleError = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   console.log(err);
 
   res.status(err instanceof ValidationError ? 400 : 500).json({
