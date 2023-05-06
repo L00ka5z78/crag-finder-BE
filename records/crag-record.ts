10cf66850e0e80bffb672bf933c78782c7962215
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-06 09:10:54 +0200

    crag record basic structure added 06.05.23 09.10

--- records/crag-record.ts
+++ records/crag-record.ts
@@ -1,1 +1,16 @@
-export class CragRecord {}
+import { CragEntity } from '../types';
+
+interface NewCragEntity extends Omit<CragEntity, 'id'> {
+  id?: string;
+}
+
+export class CragRecord implements CragEntity {
+  public id: string;
+  public name: string;
+  public description: string;
+  public url: string;
+  public lat: number;
+  public lon: number;
+
+  constructor(obj: CragEntity) {}
+}
