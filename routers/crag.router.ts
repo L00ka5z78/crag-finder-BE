6bb71e7d07c46477d0b2684780ca461dbac01d69
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-08 10:34:18 +0200

    basic endpoints created 08.05.23 10:34

--- routers/crag.router.ts
+++ routers/crag.router.ts
@@ -1,7 +1,20 @@
 import { Router } from 'express';
+import { CragRecord } from '../records/crag-record';
 
-export const cragRouter = Router().get('/', async (req, res) => {
-  res.json({
-    ok: true,
+export const cragRouter = Router()
+  .get('/search/:name?', async (req, res) => {
+    const crags = await CragRecord.listAllCrags(req.params.name ?? '');
+
+    res.json(crags);
+  })
+
+  .get('/:id', async (req, res) => {
+    const crag = await CragRecord.getOneCragById(req.params.id);
+    res.json(crag);
+  })
+
+  .post('/', async (req, res) => {
+    const newCrag = new CragRecord(req.body);
+    await newCrag.createNewCrag();
+    res.json(newCrag);
   });
-});
