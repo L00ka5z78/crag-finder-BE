52d60de87dce68827f21e83b255dc038e553c052
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-12 10:31:53 +0200

    error handler created 12.05.23 10:31

---
+++ common/middleware/error-handler.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import { CustomError } from '../errors/custom-error';
+import { ClientApiResponse } from '../../types';
+
+export const errorHandler = (
+  err: CustomError,
+  req: Request<never, ClientApiResponse<null>, never>,
+  res: Response<ClientApiResponse<null>>,
+  next: NextFunction
+): void => {
+  const status = err.status || 500;
+  const message = err.message || 'something went wrong...';
+
+  console.log({
+    status,
+    message,
+    timestamp: new Date().toISOString(),
+    path: req.url,
+  });
+
+  res.status(status).json({
+    ok: false,
+    error: message,
+    status,
+  });
+};
