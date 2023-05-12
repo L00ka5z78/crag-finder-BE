506bcaff22ebcfe924fe77c8a5f3cea896b29f65
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-12 09:24:51 +0200

    basic BE done. FE NOT CONNECTED 12.05.23 09:24

---
+++ controllers/crag-controller.ts
@@ -0,0 +1,105 @@
+import { Request, Response } from 'express';
+import {
+  AddCragRequest,
+  ClientApiResponse,
+  GetCragListResponse,
+  GetCragSingleParam,
+  GetOneCragResponse,
+  GetSearchParam,
+} from '../types';
+import { CragRecord } from '../records/crag-record';
+import { ValidationError } from '../utils/errors';
+
+export const addNewCrag = async (
+  req: Request<never, ClientApiResponse<GetOneCragResponse>, AddCragRequest>,
+  res: Response
+) => {
+  const newCrag = new CragRecord(req.body);
+  await newCrag.createNewCrag();
+
+  res.status(201).json({
+    ok: true,
+    data: newCrag,
+    status: 201,
+  });
+};
+
+export const listCrags = async (req: Request, res: Response) => {
+  const crags = await CragRecord.getAllCrags();
+
+  res.status(200).json({
+    ok: true,
+    data: crags,
+    status: 200,
+  });
+  //   res.json({
+  //     crags,
+  //   });
+};
+
+export const getSingleCragById = async (
+  req: Request<
+    GetCragSingleParam,
+    ClientApiResponse<GetOneCragResponse>,
+    never
+  >,
+  res: Response
+) => {
+  const crag = await CragRecord.getOneCragById(req.params.id);
+  res.status(200).json({
+    ok: true,
+    data: crag,
+    status: 200,
+  });
+};
+
+export const getCragsByName = async (
+  req: Request<GetSearchParam, ClientApiResponse<GetCragListResponse>, never>,
+  res: Response
+) => {
+  const crags = await CragRecord.listAllCrags(req.params.name ?? '');
+
+  res.status(200).json({
+    ok: true,
+    data: crags,
+    status: 200,
+  });
+};
+
+export const updateDetails = async (
+  req: Request<GetCragSingleParam, ClientApiResponse<GetOneCragResponse>>,
+  res: Response
+) => {
+  const crag = await CragRecord.getOneCragById(req.params.id);
+
+  if (crag === null) {
+    throw new ValidationError('Invalid ID.');
+  }
+  crag.name = req.body.name;
+  crag.description = req.body.description;
+  crag.url = req.body.url;
+  crag.routes = req.body.routes;
+  crag.lat = req.body.lat;
+  crag.lon = req.body.lon;
+
+  await crag.updateCragDetails();
+  res.status(200).json({
+    ok: true,
+    data: crag,
+    status: 204,
+  });
+
+  //   res.json(crag);
+};
+
+export const removeCrag = (
+  req: Request<GetCragSingleParam, ClientApiResponse<GetOneCragResponse>>,
+  res: Response
+) => {
+  const crag = CragRecord.deleteCrag(req.params.id);
+  res.status(200).json({
+    ok: true,
+    data: crag,
+    status: 200,
+  });
+};
--- routers/crag.router.ts
+++ routers/crag.router.ts
@@ -9,99 +9,117 @@ import {
   GetOneCragResponse,
   GetSearchParam,
 } from '../types';
+import {
+  addNewCrag,
+  getCragsByName,
+  getSingleCragById,
+  listCrags,
+  removeCrag,
+  updateDetails,
+} from '../controllers/crag-controller';
 
 export const cragRouter = Router();
 
 cragRouter
-  .get(
-    '/search/:name?',
-    async (
-      req: Request<
-        GetSearchParam,
-        ClientApiResponse<GetCragListResponse>,
-        never
-      >,
-      res
-    ) => {
-      const crags = await CragRecord.listAllCrags(req.params.name ?? '');
 
-      res.status(200).json({
-        ok: true,
-        data: crags,
-        status: 200,
-      });
+  .get('/search/:name?', getCragsByName)
+  // .get(
+  //   '/search/:name?',
+  //   async (
+  //     req: Request<
+  //       GetSearchParam,
+  //       ClientApiResponse<GetCragListResponse>,
+  //       never
+  //     >,
+  //     res
+  //   ) => {
+  //     const crags = await CragRecord.listAllCrags(req.params.name ?? '');
 
-      // res.json(crags);
-    }
-  )
+  //     res.status(200).json({
+  //       ok: true,
+  //       data: crags,
+  //       status: 200,
+  //     });
 
-  .get(
-    '/:id',
-    async (
-      req: Request<
-        GetCragSingleParam,
-        ClientApiResponse<GetOneCragResponse>,
-        never
-      >,
-      res
-    ) => {
-      const crag = await CragRecord.getOneCragById(req.params.id);
-      res.status(200).json({
-        ok: true,
-        data: crag,
-        status: 200,
-      });
+  // res.json(crags); // ******************************************************
+  //   }
+  // )
 
-      // res.json(crag);
-    }
-  )
+  .get('/:id', getSingleCragById)
 
-  .post(
-    '/',
-    async (
-      req: Request<
-        never,
-        ClientApiResponse<GetOneCragResponse>,
-        AddCragRequest
-      >,
-      res
-    ) => {
-      const newCrag = new CragRecord(req.body);
-      await newCrag.createNewCrag();
+  // .get(
+  //   '/:id',
+  //   async (
+  //     req: Request<
+  //       GetCragSingleParam,
+  //       ClientApiResponse<GetOneCragResponse>,
+  //       never
+  //     >,
+  //     res
+  //   ) => {
+  //     const crag = await CragRecord.getOneCragById(req.params.id);
+  //     res.status(200).json({
+  //       ok: true,
+  //       data: crag,
+  //       status: 200,
+  //     });
 
-      res.status(201).json({
-        ok: true,
-        data: newCrag,
-        status: 201,
-      });
-    }
-  )
+  // res.json(crag);  // remember!*******************************************************
+  //   }
+  // )
 
-  .delete('/delete/:id', async (req, res) => {
-    const crag = CragRecord.deleteCrag(req.params.id);
-    res.json(crag);
-  })
+  .post('/', addNewCrag)
 
-  .patch('/update/:id', async (req, res) => {
-    const crag = await CragRecord.getOneCragById(req.params.id);
+  // .post(
+  //   '/',
+  //   async (
+  //     req: Request<
+  //       never,
+  //       ClientApiResponse<GetOneCragResponse>,
+  //       AddCragRequest
+  //     >,
+  //     res
+  //   ) => {
+  //     const newCrag = new CragRecord(req.body);
+  //     await newCrag.createNewCrag();
 
-    if (crag === null) {
-      throw new ValidationError('Invalid ID.');
-    }
-    crag.name = req.body.name;
-    crag.description = req.body.description;
-    crag.url = req.body.url;
-    crag.routes = req.body.routes;
-    crag.lat = req.body.lat;
-    crag.lon = req.body.lon;
+  //     res.status(201).json({
+  //       ok: true,
+  //       data: newCrag,
+  //       status: 201,
+  //     });
+  //   }
+  // )
 
-    await crag.updateCragDetails();
-    res.json(crag);
-  })
+  .delete('/delete/:id', removeCrag)
+  // .delete('/delete/:id', async (req, res) => {
+  //   const crag = CragRecord.deleteCrag(req.params.id);
+  //   res.json(crag);
+  // })
 
-  .get('/', async (req, res) => {
-    const crags = await CragRecord.getAllCrags();
-    res.json({
-      crags,
-    });
-  });
+  .patch('/update/:id', updateDetails)
+  // .patch('/update/:id', async (req, res) => {
+  //   const crag = await CragRecord.getOneCragById(req.params.id);
+
+  //   if (crag === null) {
+  //     throw new ValidationError('Invalid ID.');
+  //   }
+  //   crag.name = req.body.name;
+  //   crag.description = req.body.description;
+  //   crag.url = req.body.url;
+  //   crag.routes = req.body.routes;
+  //   crag.lat = req.body.lat;
+  //   crag.lon = req.body.lon;
+
+  //   await crag.updateCragDetails();
+  //   res.json(crag);
+  // })
+
+  .get('/', listCrags);
+
+// .get('/', async (req, res) => {
+//   const crags = await CragRecord.getAllCrags();
+//   res.json({
+//     crags,
+//   });
+// });
