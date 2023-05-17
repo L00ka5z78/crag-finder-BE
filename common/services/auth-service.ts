239d7e488e9d8f7673ae4699cfd501d60e612022
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-17 14:26:41 +0200

    createAccessToken method added 17.05.23 14:26

--- common/services/auth-service.ts
+++ common/services/auth-service.ts
@@ -1,4 +1,25 @@
-export const createAccessToken = () => {};
+import { sign } from 'jsonwebtoken';
+import config from '../../config/config';
+import { IJwtPayload, ITokenData } from '../../types';
+import { CustomError } from '../errors';
+
+export const createAccessToken = (
+  currentToken: string,
+  userId: string
+): ITokenData => {
+  const expiresIn = Number(config.jsonWebToken.ACCESS_TOKEN_TIME_TO_LIVE);
+  const jwtSecretKey = config.jsonWebToken.JWT_KEY;
+  const payload: IJwtPayload = {
+    id: userId,
+    token: currentToken,
+  };
+  try {
+    const accessToken = sign(payload, jwtSecretKey, { expiresIn });
+    return { expiresIn, token: accessToken };
+  } catch (error) {
+    throw new CustomError(error.statusCode, error.message);
+  }
+};
 
 export const createRefreshToken = () => {};
 
