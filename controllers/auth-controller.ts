bdcbd2f42c9cb6d3c75a4498dd3b806b39e18e97
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-17 16:12:01 +0200

    login issue... 17.05.23 16:11

--- controllers/auth-controller.ts
+++ controllers/auth-controller.ts
@@ -1,7 +1,20 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
-import { UserRegisterRequest, UserRegisterResponse } from '../types';
-import { hashData, validateUserData } from '../utils';
-import { UserWithExistingEmailErr, serializeUserData } from '../common';
+import {
+  CookiesNames,
+  UserLoginRequest,
+  UserLoginResponse,
+  UserRegisterRequest,
+  UserRegisterResponse,
+} from '../types';
+import { checkHash, hashData, setCookie, validateUserData } from '../utils';
+import {
+  InvalidCredentialsErr,
+  UserWithExistingEmailErr,
+  createAccessToken,
+  createRefreshToken,
+  generateCurrentToken,
+  serializeUserData,
+} from '../common';
 import { UserRecord } from '../records';
 
 export const register: RequestHandler<
@@ -22,7 +35,33 @@ export const register: RequestHandler<
   res.status(201).json(serializeUserData(newUser));
 };
 
-export const login = () => {};
+export const login: RequestHandler<
+  unknown,
+  UserLoginResponse,
+  UserLoginRequest
+> = async (req, res, next) => {
+  const { email, password } = validateUserData(req);
+
+  const user = await UserRecord.getUserByEmail(email);
+  if (!user) {
+    throw new InvalidCredentialsErr();
+  }
+
+  const isMatched = await checkHash(password, user.password);
+  if (!isMatched) {
+    throw new InvalidCredentialsErr();
+  }
+  const accessTokenData = createAccessToken(
+    await generateCurrentToken(user),
+    user.id
+  );
+  const refreshTokenData = createRefreshToken(user.id);
+
+  setCookie(res, CookiesNames.AUTHORIZATION, accessTokenData);
+  setCookie(res, CookiesNames.REFRESH, refreshTokenData);
+
+  res.status(200).json(serializeUserData(user));
+};
 
 export const logout = () => {};
 
